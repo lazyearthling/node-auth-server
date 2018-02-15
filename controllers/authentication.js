710f3e757d8755ce7ae2cf71da796b48a5c6ea21
ruksad muhammed <ruksad muhammed>
2018-02-15 19:26:30 +0530

    add functionality to the authentication controller to validate email and create a new user in the database

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -5,5 +5,36 @@ const User = db.User;
 
 //SIGNUP ROUTE FUNCTION
 exports.signUp = function(req,res,next){
-  res.json({message:"signup route"})
+  const email    = req.body.email,
+        password = req.body.password;
+
+  //!IMPORTANT BEFORE DEPLOYING DO A EMAIL & PASSWORD VALIDATION CHECK//
+  //CHECK FOR EMPTY FIELDS
+  if(!email || !password){
+    return res.status(422).json({error: "You must provide both email and a password"})
+  }
+
+  //FIND THE USER
+  User.findOne({email:email})
+    .then(function(existingUser){
+      if(existingUser){
+        //IF THE USER IS ALREADY PRESENT RESPOND WITH A 422
+        res.status(422).json({error:"Email is in use"});
+      } else {
+        //CREATE NEW USER ENTRY IN THE DATABASE
+        User.create(req.body)
+          .then(function(){
+            //IF SUCCESSFULL RESPOND WITH A 201
+            res.status(201).json({message:"success"})
+          })
+          .catch(function(err){
+            //error while creating
+            return next(err);
+          })
+      }
+    })
+    .catch(function(err){
+      //error while finding user
+      return next(err);
+    })
 }
