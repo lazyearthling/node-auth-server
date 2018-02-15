bf76d3e16284ef0dc8dbf0b83933a0b541138bca
ruksad muhammed <ruksad muhammed>
2018-02-15 19:55:38 +0530

    add the crypting of password functionality using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'),
-      Schema   = mongoose.Schema;
+      Schema   = mongoose.Schema,
+      bcrypt   = require('bcrypt-nodejs');
 
 //DEFINES A USER SCHEMA
 const userSchema = new Schema({
@@ -14,6 +15,24 @@ const userSchema = new Schema({
     required : "Password cannot be left blank"
   }
 });
+
+//CRYPT USER PASSWORD BEFORE SAVING IT TO THE DATABASE USING BCRYPT
+userSchema.pre('save',function(next){
+  const user = this;const a = 0;
+
+  //Generate a salt and pass it down to .hash
+  bcrypt.genSalt(10, function(err,salt){
+    if(err) { return next(err); }
+
+    //Hash the password with the salt
+    bcrypt.hash(user.password,salt,null,function(err,hash){
+      if(err) { return next(err); }
+
+      user.password = hash;
+      next();
+    })
+  })
+})
 
 //CREATES A USER MODEL
 var UserModel = mongoose.model('User',userSchema);
